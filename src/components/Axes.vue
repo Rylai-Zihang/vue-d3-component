1f6b958366354a14279be69715250191fb14f617
zihang.he <zihang.he@aispeech.com>
2017-12-25 16:16:21 +0800

    Add comment.

--- src/components/Axes.vue
+++ src/components/Axes.vue
@@ -5,6 +5,22 @@
 
 <script>
 import * as d3 from 'd3'
+
+/**
+ * Default option of line
+ * @type {Object}
+ */
+const DEFAULTLINE = {
+  // stroke: {string},
+  // opacity: {number},
+  // lineWidth: {number},
+  lineDash: [4, 4]
+}
+
+/**
+ * Default option of grid
+ * @type {Object}
+ */
 const DEFAULTTITLE = {
   enabled: true,
   text: '',
@@ -18,13 +34,10 @@ const DEFAULTTITLE = {
   position: 'center'
 }
 
-const DEFAULTLINE = {
-  // stroke: {string},
-  // opacity: {number},
-  // lineWidth: {number},
-  lineDash: [4, 4]
-}
-
+/**
+ * Default option of label
+ * @type {Object}
+ */
 const DEFAULTLABEL = {
   offset: 0,
   textStyle: {
@@ -32,11 +45,15 @@ const DEFAULTLABEL = {
     fill: '#404040', // 文本的颜色
     fontSize: '12', // 文本大小
     fontWeight: 'bold', // 文本粗细
-    rotate: 0
+    rotate: 0 // 旋转角度
   },
   formatter (item, index) {}
 }
 
+/**
+ * Default option of grid
+ * @type {Object}
+ */
 const DEFAULTGRID = {
   enabled: false,
   lineStyle: {
@@ -49,6 +66,10 @@ const DEFAULTGRID = {
   hideLastLine: false // 是否隐藏最后一条网格线，默认为 true
 }
 
+/**
+ * Props of vue instance
+ * @type {Object}
+ */
 const props = {
   data: {
     type: Array,
@@ -103,6 +124,7 @@ const props = {
     default: function () { return {} }
   }
 }
+
 export default {
   name: 'axes',
   props,
@@ -120,15 +142,27 @@ export default {
     }
   },
   computed: {
+    /**
+     * Data length
+     * @return {[type]} [description]
+     */
     dataLength () {
       return this.data.length
     },
+    /**
+     * Size of parent node
+     * @return { width, height }
+    */
     size () {
       const parentEle = this.$el.parentNode
       const width = parentEle.getAttribute('width')
       const height = parentEle.getAttribute('height')
       return {width, height}
     },
+    /**
+     * Offset of axis component according to the position option.
+     * @return { top, left }
+     */
     offsetOpt () {
       const {width, height} = this.size
       const offsetOpt = {
@@ -148,32 +182,58 @@ export default {
       }
       return offsetOpt
     },
+    /**
+     * Max value of scale range
+     * @return {[type]} [description]
+     */
     rangeMax () {
       const range = this.scale.range()
       return d3.max(range)
     },
+    /**
+     * Max count of ticks.
+     * Determined by range & min gap.
+     * @return {[type]} [description]
+     */
     maxTickCnt () {
       const {rangeMax, minGap} = this
       return rangeMax / minGap
     },
+    /**
+     * Count of ticks in chart
+     * @return {[type]} [description]
+     */
     tickCnt () {
       const { dataLength, maxTickCnt, autoCount } = this
       let tickCnt = 0
+      // if tick count is not assigned
       if (autoCount) {
         tickCnt = dataLength > maxTickCnt ? maxTickCnt : dataLength
-      } else {
+      } else { // tick count is determined
         tickCnt = this.tickCount
       }
       return tickCnt
     },
+    /**
+     * Interval between two displayed ticks
+     * @return {[type]} [description]
+     */
     interval () {
       const {dataLength, tickCnt} = this
       return Math.ceil(dataLength / tickCnt)
     },
+    /**
+     * Array of displayed ticks
+     * @return {Array} [description]
+     */
     tickValues () {
       const {data, interval} = this
       return data.filter((d, i) => i % interval === 0)
     },
+    /**
+     * Built-in formatter for type if time
+     * @return {Function} Tick format funciton
+     */
     builtInFormatter () {
       const { type, period } = this
       let formatter = null
@@ -194,6 +254,11 @@ export default {
       }
       return formatter
     },
+    /**
+     * Rotate degree of title text.
+     * Determined by axis position.
+     * @return {[type]} [description]
+     */
     titleDegree () {
       const {position} = this
       let degree = 0
@@ -209,6 +274,11 @@ export default {
       }
       return degree
     },
+    /**
+     * Translate of title text.
+     * Determined by title position and axis position option.
+     * @return { top, left }
+     */
     titleTranslate () {
       const {titleOpt, position, size} = this
       const {offset} = titleOpt
@@ -246,6 +316,10 @@ export default {
       }
       return translate
     },
+    /**
+     * Size of grid tick.
+     * @return {[type]} [description]
+     */
     gridSize () {
       const {position, size} = this
       const {width, height} = size
@@ -264,44 +338,65 @@ export default {
     }
   },
   methods: {
+    /**
+     * Create axis (including title & grid)
+     */
     createAxis () {
       const {titleOpt, gridOpt, gridSize} = this
+      // Create axis
       const axis = d3.select(this.$el).append('g').attr('class', 'axis')
-      const axisFn = this.createAxisFn()
-      const fn = this.drawAxis()(axisFn)
+      const axisFn = this.getFn()
+      const fn = this.getAxis()(axisFn)
       axis.call(fn)
-      this.renderLabelStyle()
+      // set label style
+      this.setLabelStyle()
+      // Create axis title
       if (titleOpt.enabled) {
-        this.drawAxisTitle()
+        this.setAxisTitle()
       }
+      // Create axis grid
       if (gridOpt.enabled) {
         const grid = d3.select(this.$el).append('g').attr('class', 'grid')
         grid.call(fn.tickSize(gridSize).tickFormat(''))
-        this.renderGridStyle()
+        this.setGridStyle()
       }
     },
-    createAxisFn () {
+    /**
+     * Get initial axis function
+     * @return {Function} [description]
+     */
+    getFn () {
       const {tickOrient, scale} = this
       let axisFn = d3['axis' + tickOrient](scale)
       return axisFn
     },
-    drawAxis () {
-      const { type, renderTickPadding, formatTick } = this
+    /**
+     * Return the composition of three funcitons.
+     * Including setTickFormat, setTickPadding and getFn.
+     * @return {Function} [description]
+     */
+    getAxis () {
+      const { type, setTickPadding, setTickFormat } = this
       const { flow } = this.$lodash
-      let typeFn = null
+      let getFn = null
       switch (type) {
         case 'time':
-          typeFn = this.drawTimeAxis
+          getFn = this.getTimeAxis
           break
         case 'category':
-          typeFn = this.drawCategoryAxis
+          getFn = this.getCategoryAxis
           break
         default:
           break
       }
-      return flow(typeFn, renderTickPadding, formatTick)
+      return flow(getFn, setTickPadding, setTickFormat)
     },
-    drawTimeAxis (axisFn) {
+    /**
+     * Get axis for time axis
+     * @param  {Function} axisFn [description]
+     * @return {Function}        [description]
+     */
+    getTimeAxis (axisFn) {
       const { period, tickValues } = this
       switch (period) {
         case 'day':
@@ -318,15 +413,26 @@ export default {
       }
       return axisFn
     },
-    drawCategoryAxis (axisFn) {
+    /**
+     * Get axis for category axis
+     * @param  {Function} axisFn [description]
+     * @return {Function}        [description]
+     */
+    getCategoryAxis (axisFn) {
       const { tickValues } = this
       axisFn.tickValues(tickValues)
       return axisFn
     },
-    formatTick (axisFn) {
+    /**
+     * Format axis using built-in formatter or self-defined formatter
+     * @param  {Function} axisFn [description]
+     * @return {Function}         [description]
+     */
+    setTickFormat (axisFn) {
       const { labelOpt, builtInFormatter } = this
       const { format } = labelOpt
       let tickFormatter = null
+      // Self-defined formatter
       if (Object.prototype.toString.call(format) === '[object Function]') {
         tickFormatter = format
       } else {
@@ -335,12 +441,19 @@ export default {
       axisFn.tickFormat(tickFormatter)
       return axisFn
     },
-    renderTickPadding (axisFn) {
+    /**
+     * Set offset between axis and label text
+     * @param {Function} axisFn [description]
+     */
+    setTickPadding (axisFn) {
       const { offset } = this.labelOpt
       axisFn.tickPadding(offset)
       return axisFn
     },
-    renderLabelStyle () {
+    /**
+     * Set label style
+    */
+    setLabelStyle () {
       const { offsetOpt, labelOpt } = this
       const { textStyle } = labelOpt
       const { textAlign, fill, fontSize, fontWeight, rotate } = textStyle
@@ -355,7 +468,10 @@ export default {
       })
       axis.attr('transform', 'translate(' + offsetOpt.left + ',' + offsetOpt.top + ')')
     },
-    drawAxisTitle () {
+    /**
+     * Set axis title
+     */
+    setAxisTitle () {
       const { titleOpt } = this
       const { text } = titleOpt
       const {
@@ -374,7 +490,10 @@ export default {
       .attr('transform', `translate(${titleTranslate.left},${titleTranslate.top})rotate(${rotate + titleDegree})`)
       .text(text)
     },
-    renderGridStyle () {
+    /**
+     * Set Grid Style
+     */
+    setGridStyle () {
       const { gridOpt } = this
       const { lineStyle, hideFirstLine, hideLastLine } = gridOpt
       const { stroke, opacity, width, lineDash } = lineStyle
